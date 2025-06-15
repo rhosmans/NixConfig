3b3f2790e87d2c7ffe9dcacdd2ce0e937b2d6999
Reave Hosman <rhosmans@gmail.com>
2025-06-14 18:21:42 -0600

    content not extras

--- home.nix
+++ home.nix
@@ -61,7 +61,7 @@
                     "yarn"
                 ];
             };
-            initExtra = ''
+            initContent = ''
                 if [-f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"];
                     then source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
                 fi
