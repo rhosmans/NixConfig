a30c2ef647644e74ac0c4e813fdaaa93f98e3a6f
Reave <rhosmans@gmail.com>
2025-06-14 23:48:54 -0600

    removed if from home.nix zsh config

--- home.nix
+++ home.nix
@@ -58,11 +58,11 @@
                     "yarn"
                 ];
             };
-            initContent = ''
-                if [-f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"];
-                    then source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-                fi
-            '';
+#            initContent = ''
+#                if [-f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"];
+#                    then source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+#                fi
+#            '';
         };
         alacritty = {
             enable = true;	
