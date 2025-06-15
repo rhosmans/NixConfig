af43df608f1b4d54adb6a8e86ec6996f63686845
Reave <rhosmans@gmail.com>
2025-06-15 14:39:17 -0600

    removed comments

--- home.nix
+++ home.nix
@@ -58,11 +58,6 @@
                     "yarn"
                 ];
             };
-#            initContent = ''
-#                if [-f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"];
-#                    then source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-#                fi
-#            '';
         };
         alacritty = {
             enable = true;	
