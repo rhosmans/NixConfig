bee744f3e9f379b504c0c3eb74b98944b1476e32
Reave <rhosmans@gmail.com>
2025-06-25 00:31:44 -0600

    added link following script and right click opacity rule

--- scripts/follow-link.sh
+++ scripts/follow-link.sh
@@ -1,3 +1,3 @@
 WS=$(hyprctl clients -j | jq -r '.[] | select(.class=="zen-beta") | .workspace.id' | head -n1)
 [ -n "$WS" ] && hyprctl dispatch workspace "$WS"
-exec zen-browser "$@"
+exec zen "$@"
