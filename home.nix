132cf0e3d12d4c8b29e553b3149d526fded6a2b4
Reave <rhosmans@gmail.com>
2025-06-25 00:47:46 -0600

    dark mode

--- home.nix
+++ home.nix
@@ -35,6 +35,20 @@
             adwaita-icon-theme
     ];
 
+      gtk = {
+        enable = true;
+        theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+        };
+            gtk3.extraConfig = {
+            gtk-application-prefer-dark-theme = true;
+        };
+         gtk4.extraConfig = {
+            gtk-application-prefer-dark-theme = true;
+        };
+    };
+
     xdg.mimeApps = {
         enable = true;
         defaultApplications = {
